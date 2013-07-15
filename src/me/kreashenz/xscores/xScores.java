7a53a9e1995e9eda5713758f76be08a9a2a06350
kreashenz <izakappleid2@hotmail.com>
2013-07-15 17:40:47 +0800

    Update. xScores 1.3-

--- src/me/kreashenz/xscores/xScores.java
+++ src/me/kreashenz/xscores/xScores.java
@@ -2,11 +2,14 @@ package me.kreashenz.xscores;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 
 import net.milkbowl.vault.economy.Economy;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -24,55 +27,72 @@ import org.bukkit.scoreboard.ScoreboardManager;
 
 public class xScores extends JavaPlugin implements Listener {
 
-	public static Economy economy = null;
+	private HashMap<String, Integer> streak = new HashMap<String, Integer>();
+	private HashMap<String, Integer> kills = new HashMap<String, Integer>();
+	private HashMap<String, Integer> deaths = new HashMap<String, Integer>();
+
+	public Economy economy = null;
+	public FileConfiguration a;
+
+	private File file;
+	private FileConfiguration conf;
+
+	private xScoresCommand cmdExe;
 
 	public void onEnable() {
+		a = getConfig();
+		file = new File(getDataFolder() + File.separator + "stats.yml");
+		conf = YamlConfiguration.loadConfiguration(file);
+
+		cmdExe = new xScoresCommand(this);
+
 		setupTimer();
 		setupVault(getServer().getPluginManager());
 		getServer().getPluginManager().registerEvents(this,this);
-		getCommand("clearscores").setExecutor(new xScoresCommand(this));
-		getCommand("kdr").setExecutor(new xScoresCommand(this));
-		if(!(new File("plugins/xScores/config.yml")).exists()){
-			saveResource("config.yml", false);
-		}
+		getCommand("clearscores").setExecutor(cmdExe);
+		getCommand("kdr").setExecutor(cmdExe);
+
+		saveDefaultConfig();
+
 		try {
-			Metrics metrics = new Metrics(this);
-			metrics.start();
+			new Metrics(this).start();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+
+		runSaveTimer();
+		
+		if(!a.contains("Streak-Tag")){
+			a.set("Streak-Tag", "&aStreak&7: &c");
+			saveConfig();
+		}
+
 	}
 
 	@EventHandler
 	public void PlayerJoinEvent(PlayerJoinEvent e){
 		Player p = e.getPlayer();
 		setScoreboard(p);
-		if(!getConfig().contains(p.getName())){
-			getConfig().set(p.getName() + ".kills", "0");
-			getConfig().set(p.getName() + ".deaths", "0");
+		if(!a.contains(p.getName())){
+			a.set(p.getName() + ".kills", "0");
+			a.set(p.getName() + ".deaths", "0");
+			try {
+				conf.save(file);
+			} catch(IOException ex){
+				ex.printStackTrace();
+			}
 		}
-		saveConfig();
 	}
 
 	@EventHandler
 	public void PlayerDeathEvent(PlayerDeathEvent e){
 		Player p = e.getEntity().getPlayer();
 		Player k = p.getKiller();
-		if (k instanceof Player){
-			setDeaths(p, getDeaths(p.getName()) + 1);
-			setKills(k, getKills(k.getName()) + 1);
-			setScoreboard(p);
-			setScoreboard(k);
-		} else {
-			if(k != null){
-				getConfig().set(p.getName() + ".deaths", getDeaths(p.getName()) + 1);
-				getConfig().set(k.getName() + ".kills", getKills(k.getName()) + 1);
-			} else {
-				getLogger().info("There was no killer for " + p.getName() + "'s death.");
-			}
-			saveConfig();
-
-			setScoreboard(p);
+		if (k instanceof Player && k != null){
+			setKills(k.getName(), getKills(k.getName()) + 1);
+			setStreaks(k.getName(), getStreaks(k.getName()) +1);
+			setDeaths(p.getName(), getDeaths(p.getName()) + 1);
+			clearStreaks(p);
 		}
 	}
 
@@ -84,58 +104,96 @@ public class xScores extends JavaPlugin implements Listener {
 			Objective objective = board.registerNewObjective("test", "dummy");
 
 			objective.setDisplaySlot(DisplaySlot.SIDEBAR);
-			objective.setDisplayName(ChatColor.translateAlternateColorCodes('&', getConfig().getString("Stats-Tag")));
+			objective.setDisplayName(ChatColor.translateAlternateColorCodes('&', a.getString("Stats-Tag")));
 
-			Score kills = objective.getScore(Bukkit.getOfflinePlayer(ChatColor.translateAlternateColorCodes('&', getConfig().getString("Kills-Tag"))));
+			Score kills = objective.getScore(Bukkit.getOfflinePlayer(ChatColor.translateAlternateColorCodes('&', a.getString("Kills-Tag"))));
 			kills.setScore(getKills(p.getName()));
 
-			Score deaths = objective.getScore(Bukkit.getOfflinePlayer((ChatColor.translateAlternateColorCodes('&', getConfig().getString("Deaths-Tag")))));
+			Score deaths = objective.getScore(Bukkit.getOfflinePlayer(ChatColor.translateAlternateColorCodes('&', a.getString("Deaths-Tag"))));
 			deaths.setScore(getDeaths(p.getName()));
 
-			Score bal = objective.getScore(Bukkit.getOfflinePlayer(ChatColor.translateAlternateColorCodes('&', getConfig().getString("Balance-Tag"))));
+			Score streak = objective.getScore(Bukkit.getOfflinePlayer(ChatColor.translateAlternateColorCodes('&', a.getString("Streak-Tag"))));
+			streak.setScore(getStreaks(p.getName()));
+
+			Score bal = objective.getScore(Bukkit.getOfflinePlayer(ChatColor.translateAlternateColorCodes('&', a.getString("Balance-Tag"))));
 			bal.setScore((int) economy.getBalance(p.getName()));
-
-			p.setScoreboard(board);
-
-		} else { 
+		} else {
 
 			Objective objective = board.registerNewObjective("test", "dummy");
 			objective.setDisplaySlot(DisplaySlot.SIDEBAR);
-			objective.setDisplayName(ChatColor.translateAlternateColorCodes('&', getConfig().getString("Stats-Tag")));
+			objective.setDisplayName(ChatColor.translateAlternateColorCodes('&', a.getString("Stats-Tag")));
 
-			Score kills = objective.getScore(Bukkit.getOfflinePlayer(ChatColor.translateAlternateColorCodes('&', getConfig().getString("Kills-Tag"))));
+			Score kills = objective.getScore(Bukkit.getOfflinePlayer(ChatColor.translateAlternateColorCodes('&', a.getString("Kills-Tag"))));
 			kills.setScore(getKills(p.getName()));
 
-			Score deaths = objective.getScore(Bukkit.getOfflinePlayer((ChatColor.translateAlternateColorCodes('&', getConfig().getString("Deaths-Tag")))));
+			Score deaths = objective.getScore(Bukkit.getOfflinePlayer(ChatColor.translateAlternateColorCodes('&', a.getString("Deaths-Tag"))));
 			deaths.setScore(getDeaths(p.getName()));
 
-			p.setScoreboard(board);
+			Score streak = objective.getScore(Bukkit.getOfflinePlayer(ChatColor.translateAlternateColorCodes('&', a.getString("Streak-Tag"))));
+			streak.setScore(getStreaks(p.getName()));
 		}
+		p.setScoreboard(board);
 	}
 
 	public int getKills(String p){
-		if(getConfig().get(p + ".kills") != null){
-			return getConfig().getInt(p + ".kills");
+		if(kills.containsKey(p)){
+			return kills.get(p);
 		} else {
 			return 0;
 		}
 	}
 
 	public int getDeaths(String p){
-		return getConfig().getInt(p + ".deaths");
+		if(deaths.containsKey(p)){
+			return deaths.get(p);
+		} else {
+			return 0;
+		}
+	}
+
+	public int getStreaks(String p){
+		if(streak.containsKey(p)){
+			return streak.get(p);
+		} else {
+			return 0;
+		}
+	}
+
+	public void clearStreaks(Player p){
+		streak.put(p.getName(), 0);
 	}
 
 	public double getKDR(Player p){
-		return getConfig().getDouble(p.getName() + ".kills") / getConfig().getDouble(p.getName() + ".deaths");
+		return Double.valueOf(getKills(p.getName()) / getDeaths(p.getName()));
 	}
-	public void setKills(Player p, int Kills){
-		getConfig().set(p.getName() + ".kills", Kills);
-		saveConfig();
+	public void setKills(String p, int Kills){
+		if(!kills.containsKey(p)){
+			kills.put(p, Kills);
+		} else {
+			int a = kills.get(p);
+			kills.remove(p);
+			kills.put(p, a+1);
+		}
 	}
 
-	public void setDeaths(Player p, int Deaths){
-		getConfig().set(p.getName() + ".deaths", Deaths);
-		saveConfig();
+	public void setDeaths(String p, int Deaths){
+		if(!deaths.containsKey(p)){
+			deaths.put(p, Deaths);
+		} else {
+			int a = deaths.get(p);
+			deaths.remove(p);
+			deaths.put(p, a+1);
+		}
+	}
+
+	public void setStreaks(String p, int streaks){
+		if(!streak.containsKey(p)){
+			streak.put(p, streaks);
+		} else {
+			int a = streak.get(p);
+			streak.remove(p);
+			streak.put(p, a+1);
+		}
 	}
 
 	private void setupVault(PluginManager pm) {
@@ -150,7 +208,7 @@ public class xScores extends JavaPlugin implements Listener {
 		}
 	}
 
-	public boolean setupEconomy(){
+	private boolean setupEconomy(){
 		RegisteredServiceProvider<Economy> economyProvider = getServer().getServicesManager().getRegistration(Economy.class);
 		if (economyProvider != null) {
 			economy = economyProvider.getProvider();
@@ -168,7 +226,24 @@ public class xScores extends JavaPlugin implements Listener {
 					setScoreboard(p);
 				}
 			}
-		}, 0L, 10L);
+		}, 0L, getConfig().getInt("Scoreboard-Update-Time")*20);
+	}
+
+	private void runSaveTimer(){
+		Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(this, new Runnable(){
+			public void run(){
+				try {
+					for(Player p : Bukkit.getOnlinePlayers()){
+						conf.set(p.getName() + ".kills", getKills(p.getName()));
+						conf.set(p.getName() + ".deaths", getDeaths(p.getName()));
+
+						conf.save(file);
+					}
+				} catch (IOException e){
+					e.printStackTrace();
+				}
+			}
+		}, 0L, getConfig().getInt("Stats-File-Save-Time"));
 	}
 }
 
